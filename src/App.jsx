27d64a9800556b435bf854173f843db115a9bf3a
Firmaye <dadifirmaye@gmail.com>
2024-04-24 09:20:04 +0300

    view document and add document form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import Dashboard from "./components/dashboard"
+import { Routes, Route } from "react-router-dom";
+import Documents from "./components/Documents";
+import Dashboard from "./components/dashboard";
+import Files from "./components/Files";
 
 function App() {
-
   return (
-			<>
-        <Dashboard />
-      </>
-  )
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/documents" element={<Documents />} />
+      <Route path="/files" element={<Files />} />
+    </Routes>
+  );
 }
-
-export default App
+export default App;
---
+++ src/components/Files.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const files = () => {
+  return <div></div>;
+};
+
+export default files;
