655b08ca4ef834b6ac9b39fd105d34d23263db49
natyyG <natnalecs16@gmail.com>
2024-04-29 09:47:29 -0700

    push from localbranch

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,3 +1,26 @@
+<<<<<<< HEAD
+
+import CustomForm2 from "./components/CustomForm/CustomForm2.jsx";
+=======
+import Forum from "./components/Forum/Forum"
+import Dashboard from "./components/dashboard"
+
+>>>>>>> c6f4f98 (moving all files to frontend folder)
+function App() {
+
+  return (
+    <>
+<<<<<<< HEAD
+      <CustomForm2 />
+=======
+      <Forum />
+>>>>>>> c6f4f98 (moving all files to frontend folder)
+    </>
+  )
+}
+
+export default App
+
 
 import CustomForm2 from "./components/CustomForm/CustomForm2.jsx";
 function App() {
