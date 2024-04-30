7c2c9a7b0be3a99d033d94fa32b168382c887f48
natyyG <natnalecs16@gmail.com>
2024-04-30 10:00:56 -0700

    integrate chapa api

--- Backend/index.js
+++ Backend/index.js
@@ -17,6 +17,58 @@ const message = require('./models/messages');
 
 
 
+const CHAPA_AUTH_KEY = 'CHASECK_TEST-AluGHWd8Xjp9Ya6voAW7CERLkf6JXjEK';
+
+app.post("/accept-payment", async (req, res) => {
+  const {
+    amount,
+    currency,
+    email,
+    first_name,
+    last_name,
+    phone_number,
+    tx_ref,
+  } = req.body;
+
+ try {
+   const header = {
+     headers: {
+       Authorization: `Bearer ${CHAPA_AUTH_KEY}`,
+       "Content-Type": "application/json",
+     },
+   };
+   const body = {
+     amount: amount,
+     currency: currency,
+     email: email,
+     first_name: first_name,
+     last_name: last_name,
+     phone_number: phone_number,
+     tx_ref: tx_ref,
+    //  return_url: "http://localhost:5173/chat/6605179e271406574eb44e91",
+   }
+   let resp = "";
+   await axios
+     .post("https://api.chapa.co/v1/transaction/initialize", body, header)
+     .then((response) => {
+       resp = response;
+     })
+     .catch((error) => {
+       console.log(error.response.data); 
+       console.log(error.response.status); 
+       console.log(error.response.headers); 
+       res.status(400).json({
+         message: error,
+       });
+     });
+   res.status(200).json(resp.data);
+ } catch (e) {
+   res.status(400).json({
+     error_code: e.code,
+     message: e.message,
+   });
+ }
+});
 
 
 require("dotenv").config();
