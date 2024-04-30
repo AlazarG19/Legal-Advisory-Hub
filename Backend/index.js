2846e106f85df4662f8c68365482bab0de04bd37
natyyG <natnalecs16@gmail.com>
2024-04-30 10:04:06 -0700

    create login information validation endpoint

--- Backend/index.js
+++ Backend/index.js
@@ -89,6 +89,21 @@ app.get('/getUser/:username', (req, res) => {
         .catch(err => res.json(err));
 });
 
+app.post('/clientlogin', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = await users.findOne({ username, password });
+      if (user) {
+        res.status(200).json({ message: 'Login successful' });
+      } else {
+        res.status(401).json({ message: 'Invalid username or password' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+
 
 
 
