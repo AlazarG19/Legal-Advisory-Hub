e4de31d64ce5557535351371f76e5fcc0dd84103
natyyG <natnalecs16@gmail.com>
2024-04-30 10:47:29 -0700

    add routing to my components

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,15 +1,46 @@
-import Forum from "./components/Forum/Forum"
-import Dashboard from "./components/dashboard"
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Users from './components/Users'
+import CreateUser from './components/CreateUser'
+import UpdateUser from './components/UpdateUser'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ProjectUsersCardPane from './components/ProjectUsersCardPane'
+import UserDetails from './components/UserDetails'
+import ChatPage from './components/ChatPage'
+import CreateOffer from './components/CreateOffer'
+import OfferComponent from './components/OfferComponent'
+import Payment from './components/Payment'
+import MyComponent from './components/MyComponent'
+import MultiStepForm from './components/MultiStepForm'
+import LoginPage from './components/login'
+import FreelancerProfile from './components/freelancerProfile'
+import Clients from './components/Clients'
 function App() {
 
   return (
     <>
-      <Forum />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />}></Route>
+          <Route path="/create" element={<CreateUser/>}></Route>
+          <Route path="/update/:id" element={<UpdateUser/>}></Route>
+          <Route path="/userdetail/:id" element={<UserDetails/>}></Route>
+          <Route path="/users" element={<ProjectUsersCardPane/>}></Route>
+          <Route path="/chat/:id" element={<ChatPage />}></Route>
+          <Route path="/createOffer/:id" element={<CreateOffer />}></Route>
+          <Route path="/acceptOffer/:id" element={<OfferComponent />}></Route>
+          <Route path="/paymentPage/:id" element={<Payment />}></Route>
+          <Route path="/mycomponent" element={<MyComponent />}></Route>
+          <Route path="/multistep" element={<MultiStepForm />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/freelancerProfile" element={<FreelancerProfile/>}></Route>
+          <Route path="/clients" element={<Clients />}></Route>
+
+          
+
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
 export default App
-
-
