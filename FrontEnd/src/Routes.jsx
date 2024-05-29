8db8ce0ab2352eb38c4d12b83a6bf5334489aae4
AlazarG19 <alazargetachew70@gmail.com>
2024-05-29 23:46:31 +0300

    changed / to forum in routes.jsx

--- FrontEnd/src/Routes.jsx
+++ FrontEnd/src/Routes.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Forum from './components/Forum/Forum';
 import Answers from './components/Forum/Answers';
 import DetailPost from './components/Forum/DetailPost';
-import ManageQuestion from  './components/Forum/ManageQuestion';
+import ManageQuestion from './components/Forum/ManageQuestion';
 import ManageAnswer from './components/Forum/ManageAnswer';
 import ManageComment from './components/Forum/ManageComment';
 import TrainAI from './components/Forum/TrainAI';
@@ -11,14 +11,14 @@ import Chatbot from './components/Forum/Chatbot';
 function RRoutes() {
   return (
     <Routes>
-      <Route path="/detail/:id" element={<DetailPost/>} />
-      <Route path="/chatbot" element={<Chatbot/>} />
-      <Route path="/answers/:id" element={<Answers/>} />
-      <Route path="/" element={<Forum/>} />
-      <Route path="/mquestion" element={<ManageQuestion/>} />
-      <Route path="/mcomments/:id" element={<ManageComment/>} />
-      <Route path="/manswers/:id" element={<ManageAnswer/>} />
-      <Route path="/train" element={<TrainAI/>} />
+      <Route path="/detail/:id" element={<DetailPost />} />
+      <Route path="/chatbot" element={<Chatbot />} />
+      <Route path="/answers/:id" element={<Answers />} />
+      <Route path="/forum" element={<Forum />} />
+      <Route path="/mquestion" element={<ManageQuestion />} />
+      <Route path="/mcomments/:id" element={<ManageComment />} />
+      <Route path="/manswers/:id" element={<ManageAnswer />} />
+      <Route path="/train" element={<TrainAI />} />
 
     </Routes>
   );
