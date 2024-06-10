96b7be53855b786c0cc639ff4953332800b6658b
nati-pi <banyamano54@gmail.com>
2024-06-10 07:46:17 -0700

    chat styling

--- FrontEnd/src/components/Chat/ChatPage.jsx
+++ FrontEnd/src/components/Chat/ChatPage.jsx
@@ -194,59 +194,15 @@ const ChatPage = () => {
                     className="card-header pt-7"
                     id="kt_chat_contacts_header"
                   >
-                    {/*begin::Form*/}
-                    <form
-                      className="w-100 position-relative"
-                      autoComplete="off"
-                    >
-                      {/*begin::Icon*/}
-                      {/*begin::Svg Icon | path: icons/duotune/general/gen021.svg*/}
-                      <span className="svg-icon svg-icon-2 svg-icon-lg-1 svg-icon-gray-500 position-absolute top-50 ms-5 translate-middle-y">
-                        <svg
-                          width={24}
-                          height={24}
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <rect
-                            opacity="0.5"
-                            x="17.0365"
-                            y="15.1223"
-                            width="8.15546"
-                            height={2}
-                            rx={1}
-                            transform="rotate(45 17.0365 15.1223)"
-                            fill="currentColor"
-                          />
-                          <path
-                            d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
-                            fill="currentColor"
-                          />
-                        </svg>
-                      </span>
-                      {/*end::Svg Icon*/}
-                      {/*end::Icon*/}
-                      {/*begin::Input*/}
-                      <input
-                        type="text"
-                        className="form-control form-control-solid px-15"
-                        name="search"
-                        defaultValue=""
-                        placeholder="Search by username or email..."
-                      />
-                      {/*end::Input*/}
-                    </form>
-                    {/*end::Form*/}
                   </div>
                   {/*end::Card header*/}
                   {/*begin::Card body*/}
-                  {offer && (
+                  {offer ? (
                 <div className="offer mt-3">
                   <div className="col-md-12 col-xl-12">
                     {/*begin::Card*/}
                     <a
-                      href="../../demo1/dist/apps/projects/project.html"
+                      href="#"
                       className="card border-hover-primary"
                     >
                       {/*begin::Card header*/}
@@ -310,6 +266,63 @@ const ChatPage = () => {
                         >
                           Accept Offer
                         </Link>
+                      </div>
+                    </a>
+                  </div>
+                </div>):(<div className="offer mt-3">
+                  <div className="col-md-12 col-xl-12">
+                    {/*begin::Card*/}
+                    <a
+                      href="#"
+                      className="card border-hover-primary"
+                    >
+                      {/*begin::Card header*/}
+                      <div className="card-header border-0 pt-9">
+                        {/*begin::Card Title*/}
+                        <div className="card-title m-0">
+                          {/*begin::Avatar*/}
+                          <div className="symbol symbol-50px w-50px bg-light">
+                            <img
+                              src="/assets/media/svg/brand-logos/plurk.svg"
+                              alt="image"
+                              className="p-3"
+                            />
+                          </div>
+                          {/*end::Avatar*/}
+                        </div>
+                        {/*end::Car Title*/}
+                        {/*begin::Card toolbar*/}
+                        <div className="card-toolbar">
+                          <span className="badge badge-light-primary fw-bold me-auto px-4 py-3">
+                            {status}
+                          </span>
+                        </div>
+                        {/*end::Card toolbar*/}
+                      </div>
+                      {/*end:: Card header*/}
+                      {/*begin:: Card body*/}
+                      <div className="card-body p-9">
+                        {/*begin::Name*/}
+                        <div className="fs-3 fw-bold text-dark">{title}</div>
+                        {/*end::Name*/}
+                        {/*begin::Description*/}
+                        <p className="text-gray-400 fw-semibold fs-5 mt-1 mb-7">
+                          {description}
+                        </p>
+                        {/*end::Description*/}
+                        {/*begin::Info*/}
+                          <div className="border border-gray-300 border-dashed rounded  py-3 px-4 me-7 mb-3">
+                            <div className="fs-6 text-gray-800 fw-bold">
+                              Currently No offers Made
+                            </div>
+                          </div>
+                        <button
+                        disabled
+                          className="btn container btn-secondary mt-5"
+                          target="_blank"
+                        >
+                          Waiting Offer
+                        </button>
                       </div>
                     </a>
                   </div>
